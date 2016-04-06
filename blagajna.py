e764dbfee77cfa1e8c77f450bdcf176e7ec1a484
Miran <you@example.com>
2016-04-06 17:04:05 +0200

    komentarji3

--- blagajna.py
+++ blagajna.py
@@ -2,7 +2,7 @@
 artikli = ["banane", "datlji", "pomarance", "slive"]
 cena = [1.34, 2.45, 3.56, 4.67]
 
-print artikli
+print artikli  #da vidimo kaj imamo na razpolago
 
 izberi = raw_input("Izberi artikel:")
 
@@ -10,8 +10,8 @@ stevec = 0
 
 
 if izberi in artikli: #if zanka
-    artikli_indexs = artikli.index(izberi)
-    cena_artikla = cena[artikli_indexs]
+    artikli_indexs = artikli.index(izberi) #smo se učili pri pouku, dobimo indeks od artikla v seznamu artikli.
+    cena_artikla = cena[artikli_indexs] # in ga združimo s pripadajočo ceno iz seznama cena
     print "Cena artikla %s je: %s" %( izberi,cena_artikla)
     stevec += cena_artikla   #ceno artikla doda spremenljivki števec
     print "Tvoj racun znasa: " + str(stevec)
@@ -32,7 +32,7 @@ while (znova == "da"):   #while zanka ponavlja vnos izberi dokler...
         znova = raw_input("Ce Zelis nadaljeveti nakupovanje vtipkaj da, ce zelis zakljuciti nakup vtipkaj karkoli")  #možen izhod iz while zanke
 
     else:
-        print "Izbrani izdelek ni med artikli v ponudbi"
+        print "Izbrani izdelek ni med artikli v ponudbi" #če si se zatipkal dobiš račun in ponovno možnost 'izberi' v while zanki
         print "Tvoj racun znasa: " + str(stevec)
 
 
