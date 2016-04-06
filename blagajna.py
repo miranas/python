f1d1e6dade91d4128c8735841c8414fd5086597f
Miran <you@example.com>
2016-04-06 13:01:10 +0200

    blagajna

---
+++ blagajna.py
@@ -0,0 +1,47 @@
+# -*- encoding: utf-8 -*-
+
+artikli = ["banana", "datlji", "pomarance", "slive"]
+cena = [1.34, 2.45, 3.56, 4.67]
+
+print artikli
+izberi = raw_input("Izberi artikel:")
+
+
+stevec = 0
+
+if izberi in artikli:
+    artikli_indexs = artikli.index(izberi)
+    cena_artikla = cena[artikli_indexs]
+    print "Cena artikla %s je: %s" %( izberi,cena_artikla)
+    stevec += cena_artikla
+    print "Tvoj racun znasa: " + str(stevec)
+else:
+    print "Tega izdelka ni med artikli v ponudbi"
+
+
+znova = raw_input("Ce Zelis nadaljeveti nakupovanje vtipkaj da, ce zelis zakljuciti nakup vtipkaj karkoli")
+
+while (znova == "da"):
+    izberi = raw_input("Izberi artikel")
+    if izberi in artikli:
+        artikli_indexs = artikli.index(izberi)
+        cena_artikla = cena[artikli_indexs]
+        print "Cena artikla %s znasa %s" % (izberi ,cena_artikla)
+        stevec += cena_artikla
+        print "Tvoj racun znasa: " + str(stevec)
+
+    else:
+        print "Izbrani izdelek ni med artikli v ponudbi"
+        print "Tvoj racun znasa: " + str(stevec)
+
+
+else:
+    print "Tvoj racun znasa: " + str(stevec)
+
+
+
+
+
+
+
+
