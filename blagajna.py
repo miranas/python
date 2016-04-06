bab3fb3efa7cf6d12f191b1b15229de1f9bf559e
Miran <you@example.com>
2016-04-06 16:45:10 +0200

    komentarji

--- blagajna.py
+++ blagajna.py
@@ -6,14 +6,14 @@ print artikli
 
 izberi = raw_input("Izberi artikel:")
 
-
 stevec = 0
 
-if izberi in artikli:
+
+if izberi in artikli: #if zanka
     artikli_indexs = artikli.index(izberi)
     cena_artikla = cena[artikli_indexs]
     print "Cena artikla %s je: %s" %( izberi,cena_artikla)
-    stevec += cena_artikla
+    stevec += cena_artikla #ceno artikla doda spremenljivki števec
     print "Tvoj racun znasa: " + str(stevec)
 else:
     print "Tega izdelka ni med artikli v ponudbi"
@@ -21,15 +21,15 @@ else:
 
 znova = raw_input("Ce Zelis nadaljeveti nakupovanje vtipkaj da, ce zelis zakljuciti nakup vtipkaj karkoli")
 
-while (znova == "da"):
+while (znova == "da"): #while zanka ponavlja vnos izberi dokler...
     izberi = raw_input("Izberi artikel:")
     if izberi in artikli:
         artikli_indexs = artikli.index(izberi)
         cena_artikla = cena[artikli_indexs]
         print "Cena artikla %s znasa %s" % (izberi ,cena_artikla)
-        stevec += cena_artikla
+        stevec += cena_artikla #tale števec mora biti točno tu vmes, drugače ne dela
         print "Tvoj racun znasa: " + str(stevec)
-        znova = raw_input("Ce Zelis nadaljeveti nakupovanje vtipkaj da, ce zelis zakljuciti nakup vtipkaj karkoli")
+        znova = raw_input("Ce Zelis nadaljeveti nakupovanje vtipkaj da, ce zelis zakljuciti nakup vtipkaj karkoli")#možen izhod iz while zanke
 
     else:
         print "Izbrani izdelek ni med artikli v ponudbi"
